2b2e439c73c152816d9c3c35212b1f3a9744b13f
iraritchiemeek <iraritchiemeek@gmail.com>
2015-09-09 12:41:55 +1200

    Add post for new item

--- todo.py
+++ todo.py
@@ -1,7 +1,6 @@
 import sqlite3
-from bottle import route, run, debug, template
+from bottle import route, run, debug, template, request
 
-@route('/doesthiswork')
 @route('/todo')
 def todo_list():
     con = sqlite3.connect('todo.db')
@@ -11,6 +10,24 @@ def todo_list():
     c.close()
     output = template('make_table', rows=result)
     return output
+
+@route('/new', method='POST')
+def new_item():
+	if request.GET.get('save','').strip():
+
+	    new = request.POST.post('task', '').strip()
+	    conn = sqlite3.connect('todo.db')
+	    c = conn.cursor()
+
+	    c.execute("INSERT INTO todo (task,status) VALUES (?,?)", (new,1))
+	    new_id = c.lastrowid
+
+	    conn.commit()
+	    c.close()
+
+	    return '<p>The new task was inserted into the database, the ID is %s</p>' % new_id
+	else:
+	    return template('new_task.tpl')
 
 debug(True)
 run(reloader=True)
